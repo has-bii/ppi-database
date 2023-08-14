71a39f538ef4f90b151e3cc719aa6d0dfa10a68a
Hasbiy Robbiy <99758182+has-bii@users.noreply.github.com>
2023-08-14 12:28:29 +0300

    Seeding in one call

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Student;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,24 +14,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(1000)->create();
-
         // Student::factory(500)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
-        $this->call(RoleSeeder::class);
-        $this->call(KotaTurkiSeeder::class);
-        $this->call(AppStatusSeeder::class);
-        $this->call(EducationSeeder::class);
-        $this->call(UnivTurkiSeeder::class);
-        $this->call(KotaTurkiSeeder::class);
-        $this->call(JurusanSeeder::class);
-        $this->call(StatusSeeder::class);
-        $this->call(MyMenuSeeder::class);
-        $this->call(LinkSeeder::class);
+        $this->call([
+            RoleSeeder::class,
+            KotaTurkiSeeder::class,
+            AppStatusSeeder::class,
+            EducationSeeder::class,
+            UnivTurkiSeeder::class,
+            KotaTurkiSeeder::class,
+            JurusanSeeder::class,
+            StatusSeeder::class,
+            MyMenuSeeder::class,
+            LinkSeeder::class
+        ]);
     }
 }
